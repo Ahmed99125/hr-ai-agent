61f19c765337b2b00f6004ba06796134645a841b
Ahmed Tamer <at99125@gmail.com>
2026-08-31 15:48:27 +0300

    fix job titles

--- dist-check/check-dir.js
+++
@@ -1,7 +0,0 @@
-import { bambooHR } from "./src/services/BambooHRService.js";
-async function main() {
-    const employees = await bambooHR.getAllEmployees();
-    const emp125 = employees.find(e => e.id === "125");
-    console.log("Directory data for 125:", emp125);
-}
-main().catch(console.error);
--- dist-check/check-emp.js
+++
@@ -1,6 +0,0 @@
-import { bambooHR } from "./src/services/BambooHRService.js";
-async function main() {
-    const emp = await bambooHR.getEmployee("125");
-    console.log(emp);
-}
-main().catch(console.error);
